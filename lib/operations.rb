a87fcb75aaba3c5f2cd079868fee6c8f7e827048
Andrew Shupe <adshupe@ucdavis.edu>
2018-11-28 02:36:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed > 60
+    puts unsafe?
+  elsif speed < 40
+    puts unsafe?
+  else 
+    puts FALSE 
 end
 
-
-
 def not_safe?(speed)
-	
+speed <40 ? not_safe : FALSE
+speed >60 ? not_safe : FALSE
 end
 	
 
